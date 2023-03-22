0401ee244a77f1ba854affa801e982a7a0b750c1
Brad Caron <bacaron@utexas.edu>
2023-03-22 14:44:37 -0500

    reason: initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ap convert freesurfer parcellation
---
+++ convert.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -x
+set -e
+
+anat=`jq -r '.t1' config.json
+freesurfer=`jq -r '.freesurfer' config.json`
+parc=`jq -r '.parcellation' config.json`
+
+[ ! -d parcellation ] && mkdir parcellation
+
+[ ! -f parcellation/parc.nii.gz ] && mri_vol2vol --mov ${freesurfer}/mri/${parc}+aseg.mgz --targ ${anat} --regheader --interp nearest --o parcellation/parc.nii.gz
