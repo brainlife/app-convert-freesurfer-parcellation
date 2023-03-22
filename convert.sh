79bebf64a47122da425bc14078f35396b5c041ff
Brad Caron <bacaron@utexas.edu>
2023-03-22 16:24:09 -0500

    reason: small fix

--- convert.sh
+++ convert.sh
@@ -9,4 +9,5 @@ parc=`jq -r '.parcellation' config.json`
 
 [ ! -d parcellation ] && mkdir parcellation
 
+export SUBJECTS_DIR=./
 [ ! -f parcellation/parc.nii.gz ] && mri_vol2vol --mov ${freesurfer}/mri/${parc}+aseg.mgz --targ ${anat} --regheader --interp nearest --o parcellation/parc.nii.gz
